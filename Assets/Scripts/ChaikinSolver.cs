a37bf249518f9678b6a4eae2c6592065ffe3c26d
Antoine Pavy <antoine.pavy92@gmail.com>
2022-06-07 14:51:38 +0200

    Tracing lines

---
+++ Assets/Scripts/ChaikinSolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(GenerateByClick))]
+public class ChaikinSolver : MonoBehaviour
+{
+    private GenerateByClick generator;
+    [SerializeField] private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        generator = gameObject.GetComponent<GenerateByClick>();
+    }
+
+    private void Update()
+    {
+        if (lineRenderer != null)
+        {
+            Vector3[] positions = generator.GetPositions();
+            lineRenderer.positionCount = positions.Length;
+            lineRenderer.SetPositions(positions);
+        }
+    }
+}
